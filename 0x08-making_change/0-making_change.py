844a2f3bb2de505c7caec9e0026f61af5467eda4
P MOJAPELO <effgrandad@gmail.com>
2024-11-28 14:31:50 -0800

    fix error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
 """
-Making change
+Change making module.
 """
 
 
 def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
+    """
     if total <= 0:
         return 0
 
